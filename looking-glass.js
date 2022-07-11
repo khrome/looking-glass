0a7b49da79db8a2f3fcfa85799ffd6829fc4e88a
Abbey Sparrow <abbey@khrome.net>
2022-07-10 19:36:29 -0600

    relative path

--- looking-glass.js
+++ looking-glass.js
@@ -3,7 +3,7 @@ const LookingGlass = function(source, operations){
 }
 
 module.exports  = {
-    LookingGlass: require('src/looking-glass'),
-    LGCameraSource : require('src/camera-source'),
-    LGFrameWriter : require('src/frame-writer'),
+    LookingGlass: require('./src/looking-glass'),
+    LGCameraSource : require('./src/camera-source'),
+    LGFrameWriter : require('./src/frame-writer'),
 }
